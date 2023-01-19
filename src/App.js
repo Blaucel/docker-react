991f8fc57ab29f7040a1f987b625168d0e06b7b2
Blaucel <elecatus@gmail.com>
2023-01-19 11:28:00 +0100

    Versión final que funciona ahora si?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ¡¡Por fin se actualiza todo bonito!!
+          ¡Por fin se actualiza todo bonito!
         </p>
         <a
           className="App-link"
