d78425c025550447d266b8e01b2394b393a5f619
Blaucel <elecatus@gmail.com>
2023-02-13 16:45:53 +0100

    Saldrá como Docker?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ahora hacemos pruebas para el curro v2.1
+          Ahora hacemos pruebas para el curro v3
         </p>
         <a
           className="App-link"
