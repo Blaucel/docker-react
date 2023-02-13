58aab5396ade7f1cbd4a5f08b6cea016ed81d410
Blaucel <elecatus@gmail.com>
2023-02-13 15:53:18 +0100

    Probando tags 4, la venganza

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ¡Por fin se actualiza todo bonito! Jejeje
+          Ahora hacemos pruebas para el curro
         </p>
         <a
           className="App-link"
