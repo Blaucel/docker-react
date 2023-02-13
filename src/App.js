ea496487efe238af2fe7f96648fc073bf82b85ed
Blaucel <elecatus@gmail.com>
2023-02-13 16:50:51 +0100

    Muchas pruebas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ahora hacemos pruebas para el curro v3
+          Ahora hacemos pruebas para el curro v4
         </p>
         <a
           className="App-link"
